d5f91bd2691d202b5f551f0febef10ead41d864c
Zhyrgalbek kyzy Zhannat <zhannat.zhyrgalbek.on@gmail.com>
2022-02-01 21:16:10 +0600

    done

--- css/js/script.js
+++ css/js/script.js
@@ -1,18 +1,18 @@
-window.addEventListener("load", function () {
-    const input = document.querySelector(".input-todo");
-    const button = document.querySelector(".button");
-    const todooutput = document.querySelector("#todo-output");
 
-    input.addEventListener("click", function () {
-button.classList.add("active");
+    const form= document.querySelector("#add-todo-form");
+    const todoList = document.querySelector("#todo-output");
+    const todoInput = document.querySelector("#input-todo");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const todoItem = document.createElement("div");
+    todoItem.innerText = todoInput.value ;
+    todoItem.classList.add("todo-item");
+
+    todoList.appendChild(todoItem);
+    todoInput.value = "";
+    todoItem.addEventListener("click", function () {
+        todoItem.remove();
     });
-    todooutput.addEventListener("click", function () {
-        let outputList = document.createElement("div");
-        outputList.className = "form-items";
-        push.appendChild(outputList);
-        outputList.innerText = input.value;
-        input.value = "";
-        outputList.addEventListener("click", function () {
-        todooutput.remove("active");
-        });
-      });
+});
+
+   
